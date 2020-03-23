5122110e16ed81b5569747b4040f016428b932e1
Ariel De Los Santos <araladls@hotmail.com>
2020-03-23 16:41:43 -0400

    Contries UI page implemented

---
+++ COVIDsStat/Views/CountriesPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using COVIDsStat.ViewModels;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+using Xamarin.Forms;
+
+namespace COVIDsStat.Views
+{
+    public partial class CountriesPage : ReactiveContentPage<CountriesViewModel>
+    {
+        public CountriesPage()
+        {
+            InitializeComponent();
+            this.WhenActivated(
+                disposables =>
+                {
+                    this.OneWayBind(this.ViewModel,
+                                  vm => vm.IsBusy,
+                                  v => v.countriespage.IsBusy)
+                    .DisposeWith(disposables);
+
+                    this.OneWayBind(this.ViewModel,
+                                   vm => vm.IsBusy,
+                                   v => v.countriesList.IsRefreshing)
+                    .DisposeWith(disposables);
+
+                    this.OneWayBind(this.ViewModel,
+                                    vm => vm.Title,
+                                    v => v.countriespage.Title)
+                    .DisposeWith(disposables);
+
+                    this.OneWayBind(this.ViewModel,
+                                    vm => vm.Countries,
+                                    v => v.countriesList.ItemsSource)
+                    .DisposeWith(disposables); ;
+
+                });
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            // This is to avoid the orange on selected background color on android
+            if (e.SelectedItem == null) return;
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
