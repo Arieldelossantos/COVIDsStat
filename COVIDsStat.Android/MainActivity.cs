bbf480c7302f818426f96ce85d384b96d074d188
Ariel De Los Santos <araladls@hotmail.com>
2020-05-13 16:19:22 -0400

    Country UI fixed and changed to rounded flags on countries

--- COVIDsStat.Android/MainActivity.cs
+++ COVIDsStat.Android/MainActivity.cs
@@ -19,13 +19,16 @@ namespace COVIDsStat.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-           
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+
             CachedImageRenderer.Init(enableFastRenderer: true);
             CachedImageRenderer.InitImageViewHandler();
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
