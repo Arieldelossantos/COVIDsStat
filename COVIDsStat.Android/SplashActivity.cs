4bf1c03e8bb4f729fbfa6bd3375ab069aff61610
Ariel De Los Santos <araladls@hotmail.com>
2020-05-14 11:50:43 -0400

    splash screen created

---
+++ COVIDsStat.Android/SplashActivity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace COVIDsStat.Droid
+{
+    [Activity(Label = "COVID Stats", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+
+    }
+}
